c5f30fa4ceee7b413cc590fc87561833ee0c4ab1
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-01 18:06:10 -0300

    Tarea Memo and callback

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CallbackHook from "./components/06-memos/CallbackHook";
+// import CallbackHook from "./components/06-memos/CallbackHook";
+import { Padre } from "./components/07-tarea-memo/Padre";
 // import MemoHook from "./components/06-memos/MemoHook";
 // import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
 // import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks";
@@ -13,4 +14,4 @@ import CallbackHook from "./components/06-memos/CallbackHook";
 // import SimpleForm from "./components/02-useEffect/SimpleForm";
 // import HookApp from "./HookApp";
 
-ReactDOM.render(<CallbackHook />, document.getElementById("root"));
+ReactDOM.render(<Padre />, document.getElementById("root"));
