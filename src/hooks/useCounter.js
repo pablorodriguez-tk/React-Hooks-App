45fc1b25fc6727e13d2a093ea2a28d0df0d7842d
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-01 15:33:46 -0300

    Add UseFetch hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
 const useCounter = (initialState = 10) => {
-  const [state, setState] = useState(initialState);
+  const [counter, setCounter] = useState(initialState);
 
-  const increment = (factor = 1) => {
-    setState(state + factor);
+  const increment = () => {
+    setCounter(counter + 1);
   };
 
-  const decrement = (factor = 1) => {
-    setState(state - factor);
+  const decrement = () => {
+    setCounter(counter - 1);
   };
 
-  const reset = () => setState(initialState);
+  const reset = () => setCounter(initialState);
 
-  return { state, increment, decrement, reset };
+  return { counter, increment, decrement, reset };
 };
 
 export default useCounter;
