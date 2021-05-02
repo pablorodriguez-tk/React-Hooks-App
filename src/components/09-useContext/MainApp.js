454502ffc79d119a407aa2cb412e6f0abeadf1d1
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-01 21:48:00 -0300

    Add router

---
+++ src/components/09-useContext/MainApp.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppRouter from "./AppRouter";
+
+const MainApp = () => {
+  return <AppRouter />;
+};
+
+export default MainApp;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import CallbackHook from "./components/06-memos/CallbackHook";
 // import { Padre } from "./components/07-tarea-memo/Padre";
-import TodoApp from "./components/08-useReducer/TodoApp";
+// import TodoApp from "./components/08-useReducer/TodoApp";
+import MainApp from "./components/09-useContext/MainApp";
 // import MemoHook from "./components/06-memos/MemoHook";
 // import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
 // import MultipleCustomHooks from "./components/03-examples/MultipleCustomHooks";
@@ -15,4 +16,4 @@ import TodoApp from "./components/08-useReducer/TodoApp";
 // import SimpleForm from "./components/02-useEffect/SimpleForm";
 // import HookApp from "./HookApp";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
